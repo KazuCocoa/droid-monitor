80175a51731e902373b88ba974a0194d9c4349c2
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-05-18 23:36:28 +0900

    initial commit

---
+++ lib/droid/monitor.rb
@@ -0,0 +1,7 @@
+require "droid/monitor/version"
+
+module Droid
+  module Monitor
+    # Your code goes here...
+  end
+end
---
+++ lib/droid/monitor/version.rb
@@ -0,0 +1,5 @@
+module Droid
+  module Monitor
+    VERSION = "0.0.1"
+  end
+end
